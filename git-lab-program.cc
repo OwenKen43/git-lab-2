bda3e2a23635eddbed043658cf3246d0238f6882
Owen Kennedy <okennedy0204@gmail.com>
2026-01-21 20:09:41 -0500

    done

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Owen Kennedy
+ *        Date: January 21, 2026
+ * Description: Getting used to Githib and uploading our first file
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
